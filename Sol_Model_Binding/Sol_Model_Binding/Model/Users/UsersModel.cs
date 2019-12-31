52e718694956d6d95b07dfe503566cd55ea5aaf3
appvissta <appvissta@gmail.com>
2019-12-31 19:15:14 +0530

    Code Commit

---
+++ Sol_Model_Binding/Sol_Model_Binding/Model/Users/UsersModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sol_Model_Binding.Model.Users
+{
+    public class UsersModel
+    {
+        public String FirstName { get; set; }
+
+        public String LastName { get; set; }
+    }
+}
---
+++ Sol_Model_Binding/Sol_Model_Binding/ViewModel/Users/UsersViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+using Sol_Model_Binding.Model.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sol_Model_Binding.ViewModel.Users
+{
+    public class UsersViewModel
+    {
+
+        public UsersViewModel()
+        {
+            Users = new UsersModel();
+        }
+
+        public UsersModel Users { get; set; }
+
+        public void OnSubmit()
+        {
+            Users.FirstName = "Eshaan";
+            Users.LastName = "Naik";
+        }
+    }
+}
